f4d7b50eb7ff4f25d6fa71025043466827d96f28
Freddie Brewin <freddiebrewin@live.com>
2021-05-31 23:06:59 +0100

    Adding find all and fin one dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,85 @@
+//Install dependencies
+const router = require('express').Router(); //Importing express server
+const sequelize = require('../config/connection'); //Import sequelize database
+const { Post, User, Comment } = require('../models'); //Import models 
+const authorisation = require('../utils/auth');//Import authorisation module to redirect unauthorised users to the login page
+
+//Route for dashboard page render 
+router.get('/', authorisation, (req, res) => {
+    //Find all posts from user from the database
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id //use this sessions ID
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    modeul: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => { //Serialize the data
+        const posts = dbPostData.map(post = post.get({ plaing:true}));
+        res.render('dashboard', {posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Creates a routed allowing user to edit post
+router.get('/edit/id', authorisation, (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'commnent_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+            
+        ]
+    })
+    .then(dpPostData => { //return error if no post exists wit that id
+        if (!dpPostData) {
+            res.status(404).json({ message: 'No post exists with that id:'});
+            return;
+        }
+        const post = dpPostData.get({ plain: true});
+        res.render('edit-post', { post, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
---
+++ controllers/index.js
@@ -0,0 +1,22 @@
+//Centralise all the routes within an index.js file to export to the server
+//Import dependencies
+const router = require('express').Router(); //Connection to the server
+const apiRoutes = require('./api'); //Api routes folder
+const homeRoutes = require('./home-routes.js'); //Homepage routes file
+const dashboardRoutes = require('./dashboard-routes.js'); //Dashboard routes file
+
+//Define api routes path for the server
+router.use('./api', apiRoutes);
+
+//Define homepage path for the server
+router.use('./', homeRoutes);
+
+//Define dashboard path for the server
+router.use('./', dashboardRoutes);
+
+//ends the connection if there is a 404 response status
+router.use((res,res) => {
+    res.statusCode(404).end();
+});
+
+module.exports = router;
