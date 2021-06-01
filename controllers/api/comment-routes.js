2d72cf9cfbd6b5687c520fdf42d80bd5db33ca52
Freddie Brewin <freddiebrewin@live.com>
2021-06-01 21:58:53 +0100

    Adding comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,52 @@
 //Import dependencies
 const router = require('express').Router(); //Connection to server
 const { Comment } = require('../../models');
-const authorisation = require('../../utils/auth')
+const authorisation = require('../../utils/auth');
+
+//GET comments route
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//POST comments route
+router.postMessage('/', authorisation, (req,res) => {
+    if(req.session) {
+        Comment.create({
+            comment_text:req.body.comment_text,
+            post_id: req.body.post_id,
+            user_id: req.session.user_id
+        })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+});
+
+//DELETE comments route
+router.delete('/:id', authorisation, (req, res) => {
+    Comment.destroy({
+        where:{
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
