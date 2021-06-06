487ac6913c70933f42e20018693630d44a9ab5bd
Freddie Brewin <freddiebrewin@live.com>
2021-06-06 14:55:08 +0100

    user model

---
+++ models/user.js
@@ -0,0 +1,66 @@
+//Import Dependencies
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+        },
+        username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        }
+        },
+        email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+            }
+        },
+        password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+            }
+        }
+  },
+  {
+    // add hooks for the password hashing operation
+    hooks: {
+        // set up a beforeCreate lifecycle hook to hash the password before the object is created in the database
+        // and return the new userdata object
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+        // set up a beforeUpdate lifecycle hook to hash the password before a user object is updated in the database
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+          }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
