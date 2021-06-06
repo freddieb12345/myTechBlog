a3b0fe14ce11802bc7f4c7b34daa72e9c474cf94
Freddie Brewin <freddiebrewin@live.com>
2021-06-06 15:00:32 +0100

    add-post script

---
+++ db/scehma.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS myTechBlog_db;
+CREATE DATABASE myTechBlog_db;
---
+++ public/javascript/add-post.js
@@ -0,0 +1,27 @@
+// New Post Form Handler
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+
+    // use the add a new post POST route to add the post 
+    // user id is added from the session information in the route
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        post_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
